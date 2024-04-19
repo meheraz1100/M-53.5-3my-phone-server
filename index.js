81f0b871c69c81430c569fc7cca81de76f594fd5
meheraz1100 <meheraz1100@gmail.com>
2024-04-19 19:06:56 +0600

    project initial setup

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const phones = require('./phone.json');
+const  app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World from my Phone server is comming soon')
+})
+
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    console.log('i need data for id', id);
+    const phone = phones.find(phone => phone.id === id) || {};
+    res.send(phone)
+})
+
+app.listen(port, () => {
+    console.log(`My phone Server is running on port ${port}`)
+})
+
+
+
+
+
+
+// const express = require('express')
+// const phones = express('./phone.json')
+// const app = express()
+// const port = 3000;
+
+
+// app.get('/', (req, res) => {
+//     res.send("hello from my phone server")
+// })
+// app.get('/phones', (req, res) => {
+//     res.send(phones)
+// })
+
+// app.get('/phones/:id', (req, res) => {
+//     const id = parseInt(req.params.id)
+//     console.log('I need data for id', id)
+//     const phone = phones.find(phone => phone.id === id) || {};
+//     res.send(phone)
+// })
+
+// app.listen(port, () => {
+//     console.log(`My phone server is running on port ${port}`)
+// })
